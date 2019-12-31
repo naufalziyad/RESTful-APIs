71b1aa15b188ee4919a3b79e5574fd112f724b98
Naufal Ziyad <naufal.ziyad@detik.com>
2019-12-31 10:49:03 +0700

    [add]login action

---
+++ apis/action/login_action.go
@@ -0,0 +1,60 @@
+package action
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/naufalziyad/RESTful-APIs/apis/auth"
+	"github.com/naufalziyad/RESTful-APIs/apis/helpers/formaterror"
+	"github.com/naufalziyad/RESTful-APIs/apis/helpers/responses"
+	"github.com/naufalziyad/RESTful-APIs/apis/models"
+)
+
+func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	user.Prepare()
+	err = user.Validate("login")
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	token, err := server.SignIn(user.Email, user.Password)
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusUnprocessableEntity, formattedError)
+		return
+	}
+	responses.JSON(w, http.StatusOK, token)
+
+}
+
+func (server *Server) SignIn(email, password string) (string, error) {
+	var err error
+	user := models.User{}
+	err = server.DB.Debug().Model(models.User{}).Where("email = ?", email).Take(&user).Error
+	if err != nil {
+		return "", err
+	}
+
+	err = models.VerifyPasswordUser(user.Password, password)
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err
+	}
+	return auth.CreateToken(user.ID)
+
+}
